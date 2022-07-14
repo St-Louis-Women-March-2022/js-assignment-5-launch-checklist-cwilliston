62e5e9bf8c43849e378bc67d422fd96ca3dbb833
Carolynn Williston <carolynnwilliston@gmail.com>
2022-07-14 14:41:44 -0500

    tried moving code, didn't work. issues with pickPlanet function

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-// Write your JavaScript code here!
-//const validate = require('./scriptHelper.js');
-
-
 
 window.addEventListener("load", function() {
     let launchForm = document.getElementById("launchForm");
+
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+    });
+
+    let planet = pickPlanet(listedPlanets);
+    console.log(planet);
+    let name = planet.name;
+    let diameter = planet.diameter;
+    let star = planet.star;
+    let distance = planet.distance;
+    let moons = planet.moons;
+    let imageUrl = planet.image;
+
+    addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+    
     launchForm.addEventListener("submit", function() {
         let list = document.getElementById("faultyItems");
         let pilot = document.getElementById("pilotName").value;
@@ -13,25 +30,10 @@ window.addEventListener("load", function() {
         let cargoLevel = document.getElementById("cargoMass").value;
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
 
-        let listedPlanets;
-        // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-        let listedPlanetsResponse = myFetch();
-        listedPlanetsResponse.then(function (result) {
-            listedPlanets = result;
-            console.log(listedPlanets);
-        }).then(function () {
-            console.log(listedPlanets);
-            
-            let planet = pickPlanet(listedPlanets);
-            console.log(planet);
-            let name = planet.name;
-            let diameter = planet.diameter;
-            let star = planet.star;
-            let distance = planet.distance;
-            let moons = planet.moons;
-            let imageUrl = planet.image;
-            addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
-        })
+
+
         event.preventDefault();
     });
+
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -74,8 +74,18 @@ function pickPlanet(planets) {
 
 }
 
-module.exports.addDestinationInfo = addDestinationInfo;
-module.exports.validateInput = validateInput;
-module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
-module.exports.myFetch = myFetch;
+
+
+module.exports = {
+    addDestinationInfo: addDestinationInfo,
+    validateInput: validateInput,
+    formSubmission: formSubmission,
+    pickPlanet: pickPlanet,
+    myFetch: myFetch,
+}
+
+// module.exports.addDestinationInfo = addDestinationInfo;
+// module.exports.validateInput = validateInput;
+// module.exports.formSubmission = formSubmission;
+// module.exports.pickPlanet = pickPlanet; 
+// module.exports.myFetch = myFetch;
